388ee6ad5286a1cbed47d1ab4457261458b8ac81
Alejandro Oviedo <alejandro.oviedo.g@gmail.com>
2015-08-02 21:43:33 -0300

    added logging and changed some default html

--- index.js
+++ index.js
@@ -10,61 +10,64 @@ var users = {};
 var tweets;
 var server = http.createServer(function(req, res) {
 
-    if (req.url === '/data') {
-        if (req.method === 'GET') {
-            tweets = [];
-            res.writeHead(200, {'Content-Type': 'application/json'});
+  if (req.url === '/data') {
+    if (req.method === 'GET') {
+      console.log('Se recibió una solicitud GET del cliente.');
+      tweets = [];
+      res.writeHead(200, {
+        'Content-Type': 'application/json'
+      });
 
-            // por cada user en users, si es distinto a req.headers.user,
-            // entonces recorrer su array de tweets ->
-            // por cada tweet, preguntar si su propiedad timestamp es mayor
-            // a req.headers.timestamp, si lo es entonces agregar el tweet
-            // en cuestión al array tweets
+      // por cada user en users, si es distinto a req.headers.user,
+      // entonces recorrer su array de tweets ->
+      // por cada tweet, preguntar si su propiedad timestamp es mayor
+      // a req.headers.timestamp, si lo es entonces agregar el tweet
+      // en cuestión al array tweets
 
-
-            // responder el contenido de tweets transformado a string
-            return;
-        }
+      // responder el contenido de tweets transformado a string
+      return;
     }
+  }
 
-    var tweet;
+  var tweet;
 
-    if (req.url === '/tweet') {
-        if (req.method === 'POST') {
-            tweet = '';
-            req.setEncoding('utf8');
+  if (req.url === '/tweet') {
+    if (req.method === 'POST') {
+      console.log('Se recibió una solicitud POST del cliente.');
+      tweet = '';
+      req.setEncoding('utf8');
 
-            req.on('data', function(data) {
-                tweet += data;
-            });
+      req.on('data', function(data) {
+        tweet += data;
+      });
 
-            req.on('end', function() {
-                tweet = JSON.parse(tweet);
-                tweet.timestamp = req.headers.timestamp;
-                tweet.user = {
-                    name: req.headers.user
-                };
-                if (users[req.headers.user]) {
-                    users[req.headers.user].push(tweet);
-                } else {
-                    users[req.headers.user] = [tweet];
-                }
-
-                res.writeHead(200);
-                res.end('{}');
-            });
-            return;
+      req.on('end', function() {
+        tweet = JSON.parse(tweet);
+        tweet.timestamp = req.headers.timestamp;
+        tweet.user = {
+          name: req.headers.user
+        };
+        if (users[req.headers.user]) {
+          users[req.headers.user].push(tweet);
+        } else {
+          users[req.headers.user] = [tweet];
         }
-    }
 
-    // parte dónde sirvo contenido estático
-    serve(req, res, function() {
-        res.end();
-    });
+        res.writeHead(200);
+        res.end('{}');
+      });
+      return;
+    }
+  }
+
+  // parte dónde sirvo contenido estático
+  serve(req, res, function() {
+    res.end();
+  });
 });
 
 var port = argv.port || process.env.PORT || process.env.port || process.env.OPENSHIFT_NODEJS_PORT || 8000;
 
 server.listen(port, argv.ip || process.env.OPENSHIFT_NODEJS_IP, function() {
-    console.log('Server is now listening at port: ' + port);
+  console.log('Server is now listening at port: ' + port);
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,54 @@
 $(document).ready(function() {
-    var lastTime = 0;
-    var dialog = document.getElementById('dialog');
-    dialog.showModal();
+  var lastResponse = 0;
+  var dialog = document.getElementById('dialog');
+  var ingresarBtn = $('#ingresarBtn');
+  var tweetBtn = $('#tweetBtn');
+  var tweetContent = $('#tweetContent');
+  var userInput = $('#userInput');
+  var contenedorDeTweets = $('#contenedor .centre');
+  var userName;
 
-    $('#ingresarBtn').on('click', function() {
-        dialog.close();
-        $('#userName').text($('#userInput').val());
-    });
+  dialog.showModal();
 
-    $('#tweet').on('click', function() {
-        // hacer POST a /tweet
-    });
+  // modal inicial
+  ingresarBtn.on('click', function() {
+    dialog.close();
+    userName = userInput.val();
+    $('#userName').text(userName);
+  });
 
-    // Cada X cantidad de segundos, hacer un GET a /data
-    // Por cada elemento de la lista que responda el servidor,
-    // agregarlo haciendo $('#contenedor .centre').append(htmlDelTweet)
-    // HINT: usar la function getHTMLforTweet, que recibe un objeto que representa
-    // un tweet y devuelve un objeto que representa su HTML
-
-    function getHTMLforTweet(tweet) {
-        var elemHTML = $('<div></div>');
-        elemHTML.html('User:' + tweet.user.name +
-            '<br>' + tweet.text);
-        return elemHTML;
+  // si el usuario apreta enter en el modal cuenta cómo click
+  userInput.keyup(function(event) {
+    if (event.keyCode == 13) {
+      ingresarBtn.click();
     }
+  });
+
+  // si el usuario apreta enter en el input del tweet, lo tweetea
+  tweetContent.keyup(function(event) {
+    if (event.keyCode == 13) {
+      tweetBtn.click();
+    }
+  });
+
+  tweetBtn.on('click', function() {
+    // hacer post a /tweet con el formato
+    // {
+    //     text:'el contenido del input tweetContent'
+    // }
+  });
+
+  // Cada X cantidad de segundos, hacer un GET a /data
+  // Por cada elemento de la lista que responda el servidor,
+  // agregarlo haciendo $('#contenedor .centre').append(htmlDelTweet)
+  // HINT: usar la function getHTMLforTweet
+
+  // función que recibe un objeto que represente a un tweet
+  // y devuelve un elemento html que represente a ese tweet
+  function getHTMLforTweet(tweet) {
+    var elemHTML = $('<div></div>');
+    elemHTML.html('<span>User:' + tweet.user.name +
+      '</span><p>' + tweet.text + '</p>');
+    return elemHTML;
+  }
 });
